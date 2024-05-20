3f1f24fdae1eb316cb9b53e6d3aae7b256298cf6
Sikoo54 <sachas4f@gmail.com>
2024-05-20 15:04:52 +0800

    smoothness

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
+import "../styles/components/work.css";
 import "../styles/utils.css";
+import "../styles/lenis.css";
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
