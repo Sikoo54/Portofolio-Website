d09ec89e4c845e1eaf85e24c3992ea917f2f5237
Sikoo54 <sachas4f@gmail.com>
2024-07-21 20:43:02 +0800

    contact section added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/work.css";
 import "../styles/components/contact.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
 import "../styles/lenis.css";
 
